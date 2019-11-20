60cfae232e874863382fb7ddcf1342dbbffb9f87
Hugo Moran <dev.hugomoran@gmail.com>
2019-11-20 16:27:47 -0300

    Basket now behaves like one.

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Get the current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        // The Camera's Z position sets how far to push the mouse
+        // into 3D
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        // Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // Move the X position of this Basket to the X position of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos; 
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
     }
 }
