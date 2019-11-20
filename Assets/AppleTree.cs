fc25af9e874ab3b7d3a593155767f38019d40972
Hugo Moran <dev.hugomoran@gmail.com>
2019-11-20 12:27:24 -0300

    AppleTree now changes directions randomly.

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 public class AppleTree : MonoBehaviour
 {
     [Header("Set in Inspector")]
-    // Prefab for instantiating apples
+    // Prefab for instantiating apples.
     public GameObject applePrefab;
 
-    // Speed at which the AppleTree moves
+    // Speed at which the AppleTree moves.
     public float speed = 1f;
 
-    // Distance where AppleTree turns around
+    // Distance where AppleTree turns around.
     public float leftAndRightEdge = 10f;
 
-    // Chance that the AppleTree will change directions
+    // Chance that the AppleTree will change directions.
     public float changeToChangeDirections = 0.1f;
 
-    // Rate at which Apples will be instantiated
+    // Rate at which Apples will be instantiated.
     public float secondsBetweenAppleDrops = 1f;
 
 
     void Start()
     {
-        // Dropping apples every second
+        // Dropping apples every second.
     }
 
 
@@ -43,5 +43,15 @@ public class AppleTree : MonoBehaviour
             speed = -Mathf.Abs(speed); // Move left.
         }
         
+    }
+
+    void FixedUpdate()
+    {
+        // Changing directions randomly is now time-based because
+        // of FixedUpdate().
+        if (Random.value < changeToChangeDirections)
+        {
+            speed *= -1; // Change direction.
+        }
     }
 }
